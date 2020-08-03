fe56678ef3fc4ed33a78fcd32144774955698868
juandsuarezz <1486@holbertonschool.com>
2020-08-03 21:07:16 +0000

    [Fix]: Puppet comand

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
-# Executes a kill command to a process called killmenow
-exec { 'pkill killmenow':
-     command  => 'pkill -f killmenow',
-     provider => 'shell',
+#Executes a kill command to a process called killmenow
+exec { 'pkill':
+  command  => 'pkill -f killmenow',
+  provider => 'shell',
 }
