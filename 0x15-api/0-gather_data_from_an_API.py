f09cd345026ec8f8feb4ace104a80ecd0ac7870c
juandsuarezz <1486@holbertonschool.com>
2020-09-14 19:50:49 +0000

    [Add]: First update

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Pings a To-Do API for data and displays tasks completed and tasks done for a specified user"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    employee_id = argv[1]
+    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
+    url_user = 'https://jsonplaceholder.typicode.com/users/'
+    todo = requests.get(url_todo, params={'userId': employee_id})
+    user = requests.get(url_user, params={'id': employee_id})
+
+    todo_dict_list = todo.json()
+    user_dict_list = user.json()
+
+    done_tasks = []
+    total_tasks = len(todo_dict_list)
+    employee = user_dict_list[0].get('name')
+
+    for task in todo_dict_list:
+        if task.get('completed') is True:
+            done_tasks.append(task)
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee, len(done_tasks), total_tasks))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Pings a To-Do API for data on a specified user and saves the data to a CSV file"""
+import csv
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    employee_id = argv[1]
+    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
+    url_user = 'https://jsonplaceholder.typicode.com/users/'
+    todo = requests.get(url_todo, params={'userId': employee_id})
+    user = requests.get(url_user, params={'id': employee_id})
+
+    todo_dict_list = todo.json()
+    user_dict_list = user.json()
+
+    employee = user_dict_list[0].get('username')
+
+    with open("{}.csv".format(employee_id), "a+") as csvfile:
+        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo_dict_list:
+            status = task['completed']
+            title = task['title']
+            csvwriter.writerow(["{}".format(employee_id),
+                                "{}".format(employee),
+                                "{}".format(status),
+                                "{}".format(title)])
