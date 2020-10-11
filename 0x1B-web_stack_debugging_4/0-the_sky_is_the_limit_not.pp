6ab8cdebee6b34931b3863a1923a712ff7baec56
Juan David Suarez <60364795+juandsuarezz@users.noreply.github.com>
2020-10-11 06:37:18 -0500

    Create 0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Bring the limit higher for the open file of nginx
+exec { 'higher limit':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
